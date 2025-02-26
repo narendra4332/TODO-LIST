4ba4f9022223eed8bc16a7e25745137570223603
Narendra <narendralodhi862@gmail.com>
2025-02-26 17:02:04 +0530

    eddit containet

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <div className='bg-dark text-light'>
             <p className='text-center py-3'>
-                Copyright & copy; MyTodoList.com
+                Copyright &copy; MyTodoList.com
             </p >
         </div >
     )
