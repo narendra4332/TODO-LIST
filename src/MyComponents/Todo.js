9a3bde4bb9834d425f2a066b4bd54e1342cf0461
Narendra <narendralodhi862@gmail.com>
2025-02-26 17:08:17 +0530

    add css in delete button

--- src/MyComponents/Todo.js
+++ src/MyComponents/Todo.js
@@ -6,7 +6,7 @@ export const Todo = ({ todo, onDelete }) => {
             <div className='py-2'>
                 <h4>{todo.title}</h4>
                 <p>{todo.desc}</p>
-                <button className="btn btn-sm btn-danger" onClick={() => { onDelete(todo) }}> Delet</button>
+                <button className="btn btn-sm btn-danger" style={{padding:"5px 15px",fontSize:"15px"}} onClick={() => { onDelete(todo) }}> Delete</button>
             </div >
             <hr />
         </>
